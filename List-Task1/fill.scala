34d092282b35044540f80b7c1a31fac6f2362592
ajey <71833053+ajay9758@users.noreply.github.com>
2021-10-29 11:58:44 +0530

    Create fill.scala

---
+++ List-Task1/fill.scala
@@ -0,0 +1,15 @@
+object HelloWorld {
+	def main(args: Array[String]): Unit = {
+  
+  var a = List.fill(5)("abc")
+  
+  var b = List.fill(5)(2)
+  
+  println(a)
+  
+  println(b)
+  
+  
+  
+	}
+}
