af36b5e3f268e2dd28c8b36db2464bd2c49abac1
ajey <71833053+ajay9758@users.noreply.github.com>
2021-10-29 11:43:04 +0530

    Create initial.scala

---
+++ List-Task1/initial.scala
@@ -0,0 +1,14 @@
+object HelloWorld {
+	def main(args: Array[String]): Unit = {
+  //lists are immutable ane represents linked list
+  var l1: List[String] = List["asdf","qwerty","zxcv"];
+  
+  var l2 = "abc" :: ("fgh" :: ("qwre" :: Nil  ));
+  
+  println(l1);
+  
+  println(l2);
+  
+  
+	}
+}
